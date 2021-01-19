77d6921505a7ec87c8b5f85b75fe103e38234d03
Alex Sircu <alex.sricu@gmail.com>
2021-01-19 20:28:09 +0100

    Aggiunto GET

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ Creare una variabile con un paragrafo di testo.
 Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre asterischi.
 */
 $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+$badword = $_GET["badword"];
  ?>
 
 <!DOCTYPE html>
@@ -16,6 +17,6 @@ $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmo
   <body>
     <h2>Testo con variabile php: <?php echo $text; ?> </h2>
     <h4>La lunghezza del testo è di: <?php echo strlen($text); ?> caratteri.</h4>
-    <h2>Testo censurato: <?php echo str_replace("ut", "***", $text); ?></h2>
+    <h2>Testo censurato: <?php echo str_replace($badword, "***", $text); ?></h2>
   </body>
 </html>
